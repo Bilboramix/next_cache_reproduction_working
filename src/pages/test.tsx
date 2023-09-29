e2346ee9238a06048b915fdeed1cfcd723de2095
Axel Catois - CodingBill <contact@codingbill.com>
2023-09-30 00:11:04 +0200

    init

---
+++ src/pages/test.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export const getServerSideProps = async () => {
+  console.log("\nGET_SERVER_SIDE_PROPS FIRED !");
+  return {
+    props: { test: new Date().getTime() },
+  };
+};
+
+export default function Test({ test }: { test: string }) {
+  return (
+    <>
+      <Link href="/">Home</Link>
+      <Link href="/test">test</Link>
+      <p>Test - {test}</p>
+    </>
+  );
+}
